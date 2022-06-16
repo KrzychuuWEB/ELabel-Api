72af2c149524ed825bc2676a753333aad970f607
Krzysiek <krzychuuweb@gmail.com>
2022-06-16 11:42:20 +0200

    First commit

---
+++ src/main/java/pl/krzychuuweb/elabel/ElabelApplication.java
@@ -0,0 +1,13 @@
+package pl.krzychuuweb.elabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/krzychuuweb/elabel/ElabelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krzychuuweb.elabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
